fa456fe85e1619ed1ae2ac69e162f694834ba56b
Gabriel Ribeiro <bielzin.senac2024@gmail.com>
2024-12-09 17:08:25 -0300

    JaVA

---
+++ ClinicaOdontologica/src/OdontoVida/Dentista.java
@@ -0,0 +1,64 @@
+package OdontoVida;
+
+import java.util.ArrayList;
+
+public class Dentista {
+	private String nome;
+	private String crm;
+	private String email;
+	private String telefone;
+	private ArrayList<Consulta> consulta;
+	
+	public Dentista (String nome, String crm, String email, String telefone){
+		this.nome = nome;
+		this.crm = crm;
+		this.email = email;
+		this.telefone = telefone;
+		this.consulta = new ArrayList<>();
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public String getCrm(){
+		return crm;
+	}
+	
+	public void setCrm(String crm){
+		this.crm = crm;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String getTelefone(){
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone){
+		this.telefone = telefone;
+	}
+	
+	public ArrayList<Consulta> getConsulta(){
+		return consulta;
+	}
+	
+	public void adicionarAgendamento(Consulta consulta) { 
+		this.consulta.add(consulta);
+	}
+	
+	@Override
+	public String toString(){
+		return "Dentista[Nome: " + nome + ", CRM: "+ crm + ", Email: "+ email + "e Telefone: " + telefone +"]";
+	}
+}
